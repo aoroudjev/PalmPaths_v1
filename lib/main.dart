c97675d9b17bcf3acce360099d292832a9ef4e34
Alexander Oroudjev <a.oroudjev@gmail.com>
2024-05-02 02:09:42 -0700

    Move all ui to separate files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:static_image_test/ui/detection_widget.dart';
-import 'dart:io';
+import 'package:static_image_test/ui/menu_widget.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -12,126 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.orange,
-        ),
       ),
       home: const MainMenu(),
-    );
-  }
-}
-
-// TODO: Extract to separate file.
-class MainMenu extends StatelessWidget {
-  const MainMenu({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFDAD3C1),
-      appBar: AppBar(
-        title: const Text('Main Menu'),
-        backgroundColor: Colors.black.withOpacity(0.5),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Main logo
-            Image.asset(
-              'assets/images/hand_logo_large.png',
-              height: 350,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            // Buttons with padding
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const DetectorWidget()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(200, 50),
-                  backgroundColor: Color(0xFF918B7D),
-                  elevation: 10,
-                ),
-                child: const Text(
-                  'Start',
-                  style: TextStyle(fontSize: 30, color: Color(0xFF1D1B17)),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const HistoryScreen()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(200, 50),
-                  backgroundColor: Color(0xFF918B7D),
-                  elevation: 10,
-                ),
-                child: const Text(
-                  'History',
-                  style: TextStyle(fontSize: 30, color: Color(0xFF1D1B17)),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  exit(0);
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(200, 50),
-                  backgroundColor: Color(0xFF918B7D),
-                  elevation: 10,
-                ),
-                child: const Text(
-                  'Quit',
-                  style: TextStyle(fontSize: 30, color: Color(0xFF1D1B17)),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// TODO: Implement History element (Save past readings)
-class HistoryScreen extends StatelessWidget {
-  const HistoryScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('History'),
-        backgroundColor: Colors.black.withOpacity(0.5),
-      ),
-      body: const Center(
-        child: Text('History Screen'),
-      ),
     );
   }
 }
